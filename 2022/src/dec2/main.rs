36dcca89d55d79606b50659c3d620916ee40643a
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2022-12-09 20:06:56 +0100

    merge Dec 2 binaries for consistency

--- 2022/src/dec2/main.rs
+++ 2022/src/dec2/main.rs
@@ -13,18 +13,37 @@ enum Outcome {
     Win,
 }
 
+fn symbol_that_wins_against(x: Symbol) -> Symbol {
+    match x {
+        Symbol::Rock => Symbol::Paper,
+        Symbol::Paper => Symbol::Scissors,
+        Symbol::Scissors => Symbol::Rock,
+    }
+}
+
+fn symbol_that_loses_against(x: Symbol) -> Symbol {
+    match x {
+        Symbol::Paper => Symbol::Rock,
+        Symbol::Scissors => Symbol::Paper,
+        Symbol::Rock => Symbol::Scissors,
+    }
+}
+
 fn outcome_for_me(me: Symbol, them: Symbol) -> Outcome {
-    match (me, them) {
-        (Symbol::Rock, Symbol::Scissors) => Outcome::Win,
-        (Symbol::Rock, Symbol::Paper) => Outcome::Loss,
-        (Symbol::Paper, Symbol::Rock) => Outcome::Win,
-        (Symbol::Paper, Symbol::Scissors) => Outcome::Loss,
-        (Symbol::Scissors, Symbol::Paper) => Outcome::Win,
-        (Symbol::Scissors, Symbol::Rock) => Outcome::Loss,
-        _ => {
-            assert!(me == them);
-            Outcome::Tie
-        }
+    if symbol_that_wins_against(me) == them {
+        Outcome::Loss
+    } else if symbol_that_wins_against(them) == me {
+        Outcome::Win
+    } else {
+        Outcome::Tie
+    }
+}
+
+fn symbol_to_choose(opponent: Symbol, desired_outcome: Outcome) -> Symbol {
+    match desired_outcome {
+        Outcome::Tie => opponent,
+        Outcome::Win => symbol_that_wins_against(opponent),
+        Outcome::Loss => symbol_that_loses_against(opponent),
     }
 }
 
@@ -46,11 +65,12 @@ fn round_score(me: Symbol, them: Symbol) -> i32 {
 }
 
 fn main() -> Result<()> {
-    let mut running_total: i32 = 0;
+    let mut running_total_interpretation_one: i32 = 0;
+    let mut running_total_interpretation_two: i32 = 0;
 
     for line in std::io::stdin().lines() {
         let line = line?;
-        let (them, me) = line.split_once(" ").unwrap();
+        let (them, second_column) = line.split_once(" ").unwrap();
         let them = match them {
             "A" => Symbol::Rock,
             "B" => Symbol::Paper,
@@ -58,17 +78,34 @@ fn main() -> Result<()> {
             _ => panic!("bad symbol for them"),
         };
 
-        let me = match me {
+        let interpretation_one_me = match second_column {
             "X" => Symbol::Rock,
             "Y" => Symbol::Paper,
             "Z" => Symbol::Scissors,
             _ => panic!("bad symbol for me"),
         };
 
-        running_total += round_score(me, them)
+        let interpretation_two_desired_outcome = match second_column {
+            "X" => Outcome::Loss,
+            "Y" => Outcome::Tie,
+            "Z" => Outcome::Win,
+            _ => panic!("bad symbol for desired outcome"),
+        };
+
+        let interpretation_two_me = symbol_to_choose(them, interpretation_two_desired_outcome);
+
+        running_total_interpretation_one += round_score(interpretation_one_me, them);
+        running_total_interpretation_two += round_score(interpretation_two_me, them);
     }
 
-    println!("Total score: {}", running_total);
+    println!(
+        "Total score (interpretation one): {}",
+        running_total_interpretation_one
+    );
+    println!(
+        "Total score (interpretation two): {}",
+        running_total_interpretation_two
+    );
 
     Ok(())
 }
